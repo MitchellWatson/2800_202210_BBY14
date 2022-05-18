bc63b424c24830b3112c00b95b829511b8777ed7
Basil Kim <kimbasillio@gmail.com>
2022-05-17 19:32:29 -0700

    made a motivational easter egg feature activated by clicking the logo

--- public/scripts/easterEgg.js
+++ public/scripts/easterEgg.js
@@ -1,18 +1,29 @@
 const button = document.getElementById("secret-click");
 const toasts = document.getElementById('toasts')
-
+let count = 0;
 const messages = [
-    'Message One', 
-    'Message Two',
-    'Message Three',
-    'Message Four'
+    'Age is just a number for those who know how to make the most of their lives', 
+    'Anyone who keeps learning stays young',
+    'Enjoy your day, you deserve it',
+    'The spirit never ages. It stays forever young',
+    'Wrinkles should merely indicate where smiles have been',
+    'You’re only as old as you feel',
+    'With old age comes wisdom… and discounts!'
 ]
 
-const types = ['info', 'success', 'error']
+const types = ['purple', 'green', 'red']
 
-button.addEventListener('click', () => createNotification())
+button.addEventListener('click', () => {
+    count += 1;
+    if (count < 3) {
+    createNotification()
+    } else {
+        count = 0;
+    }
+})
 
 function createNotification(message = null, type = null) {
+
     const notif = document.createElement('div')
     notif.classList.add('toast')
     notif.classList.add(type ? type : getType())
@@ -20,10 +31,15 @@ function createNotification(message = null, type = null) {
     notif.innerText = message ? message : getMessage()
 
     toasts.appendChild(notif)
+    
+
 
     setTimeout(() => {
         notif.remove()
     }, 3000)
+
+
+    
 }
 
 function getMessage() {
