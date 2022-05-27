084315a43270041974218a2cb41f4a64a9da23e6
MitchellWatson <mwatson75@my.bcit.ca>
2022-05-27 10:05:47 -0700

    accidentally reverted gabify.js to old changes. Reverting back'

--- gabify.js
+++ gabify.js
@@ -66,13 +66,17 @@ app.use(bodyparser.urlencoded({
     extended: true
 }))
 
-// --- Local hosting ---
+/**
+ * Local Hosting
+ */
 // const dbHost = "127.0.0.1";
 // const dbUser = "root";
 // const dbPassword = "";
 // const dbName = "comp2800";
 
-// --- Heroku hosting ---
+/**
+ * Heroku Hosting
+ */
 const dbHost = "us-cdbr-east-05.cleardb.net";
 const dbUser = "b959a83957277c";
 const dbPassword = "5e9f74c2";
@@ -632,7 +636,7 @@ app.get("/contact", async function (req, res) {
                             users += '</div>'
                             users +=
                                 '<div class="button">' +
-                                '<a href= "/gabChat" target="' + finalUsers[i].ID + '" class="option add"><span class="material-symbols-outlined">sms</span>Chat</a>' +
+                                '<a href= "/gabChat" target="' + finalUsers[i].ID + '" class="option add">Chat</a>' +
                                 '</div>' +
                                 '</div>';
                             usersProfiles.innerHTML += users;
@@ -1435,7 +1439,7 @@ app.get("/chatGlobalSelect", function (req, res) {
     }
 });
 
-// code for one-to-one chat; work in progress
+// code for one-to-one chat;
 io.on('connection', socket => {
     socket.on('joinRoom', ({ chatUser }) => {
         const user = newUser(socket.id, chatUser);
@@ -1502,11 +1506,3 @@ app.get("/gabChat", function (req, res) {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT);
-
-// //For Milestone hand-ins:
-// let port = 8000;
-// app.listen(port, function () {
-// });
-
-//For Heroku deployment
-// app.listen(process.env.PORT || 3000);
